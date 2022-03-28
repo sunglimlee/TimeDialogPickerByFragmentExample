2bfa90e8931cb0b613402d4fd158c7b9d5193e29
Steve Lee <steve.patriot@gmail.com>
2022-03-28 10:48:28 -0400

    그냥 comment만 썼슴.

--- app/src/main/java/com/example/timedialogpickerbyfragmentexample/TimePickerFragment.kt
+++ app/src/main/java/com/example/timedialogpickerbyfragmentexample/TimePickerFragment.kt
@@ -7,6 +7,7 @@ import android.text.format.DateFormat
 import androidx.fragment.app.DialogFragment import java.util.*
 
 //그래서 DialogFragment를 사용하는구나..
+//사용하기는 DialogFragment를 사용하는데 화면에 실제로 보여지는 다이얼로그는 TimePickerDialog를 사용하는구나..
 class TimePickerFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val c : Calendar = Calendar.getInstance()
